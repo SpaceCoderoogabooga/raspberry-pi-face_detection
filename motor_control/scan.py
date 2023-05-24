e7bd024cf9947066a8408392a048ca20cfcbc939
VovoRVA <rokotyanskiiv@gmail.com>
2023-05-24 20:15:14 -0300

    merging face lidar functionality with x motor movement

--- motor_control/scan.py
+++ motor_control/scan.py
@@ -9,19 +9,20 @@ def map_room():
     current_y_position = 0
     delta_x = 0.1
     delta_y = 0
+    x_range = range(3, 9, 0.1)
+    print(x_range)
 
     motor_functions.change_position(current_x_position, current_y_position)
     map = {}
     time.sleep(15)
-    print('starting mapping')
-    while True:
+    for x_position in x_range:
         current_x_position += delta_x
         current_y_position += delta_y
-        motor_functions.change_position(current_x_position, current_y_position)
+        motor_functions.change_position(x_position, current_y_position)
+        time.sleep(2)
         distance = range_finder.distance()
+        print(distance)
         map[current_x_position] = distance
-        print(map)
 
-motor_functions.change_position(0, 0)
 map_room()
 motor_functions.change_position(0, 0)
